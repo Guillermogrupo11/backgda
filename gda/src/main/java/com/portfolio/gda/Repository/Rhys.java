ba77af158e39c21f90a4919a99ae1c5c40d23040
Guillermo <guillermo.arena@gmail.com>
2022-10-04 19:56:41 -0300

    haciendolo en intellij

---
+++ gda/src/main/java/com/portfolio/gda/Repository/Rhys.java
@@ -0,0 +1,15 @@
+package com.portfolio.gda.Repository;
+
+import com.portfolio.gda.Entity.hys;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author guill
+ */
+public interface Rhys extends JpaRepository<hys, Integer>{
+    Optional<hys> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+}
+
